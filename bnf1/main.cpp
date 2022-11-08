e379916c7df3205051ec0150156147da5f403195
smichajlidis <smichajlidis@gmail.com>
2022-11-08 21:01:43 +0100

    Change if else-if to switch

--- bnf1/main.cpp
+++ bnf1/main.cpp
@@ -2,46 +2,24 @@
 
 using namespace std;
 
-int palindrom(int);
-
 int main() {
-
-    int t, n;
-    int count{ 0 };
-
-    do {
-        cin >> t;
-    } while (t > 80);
-
-    for (int i{ 0 }; i < t; i++) {
-
-        do {
-            cin >> n;
-        } while (n < 1 || n > 80);
-
-        if (n < 10)
-            cout << n << " 0\n";
-        else if (n == 29 || n == 38 || n == 47 || n == 56 || n == 65 || n == 74)
-            cout << "121 1\n";
-        else if (n == 19 || n == 28 || n == 37 || n == 46 || n == 64 || n == 73)
-            cout << "121 2\n";
-        else if (n == 39 || n == 48 || n == 57 || n == 75)
-            cout << "363 2\n";
-        else if (n == 49 || n == 58 || n == 67 || n == 76)
-            cout << "484 2\n";
-        else if (n == 69 || n == 78)
-            cout << "4884 4\n";
-        else if (n == 68 || n == 59)
-            cout << "1111 3\n";
-        else if (n == 79)
-            cout << "44044 6\n";
-        else if (n == 11 || n == 22 || n == 33 || n == 44 || n == 55 || n == 66 || n == 77)
-            cout << n << " 0\n";
-        else if (n%10 == 0)
-            cout << n+(n/10) << " 1\n";
-        else
-            cout << ((n/10)+(n%10))*10+((n/10)+(n%10)) << " 1\n";
+    int t, n, count {0};
+    std::cin>>t;
+    for(int i {0}; i<t; i++) {
+        cin >> n;
+        switch (n) {
+            case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: std::cout<<n<<" 0\n"; break;
+            case 10: case 20: case 30: case 40: case 50: case 60: case 70: case 80: std::cout<<n+(n/10)<<" 1\n"; break;
+            case 11: case 22: case 33: case 44: case 55: case 66: case 77: std::cout<<n<<" 0\n"; break;
+            case 29: case 38: case 47: case 56: case 65: case 74: std::cout<<"121 1\n"; break;
+            case 19: case 28: case 37: case 46: case 64: case 73: std::cout<<"121 2\n"; break;
+            case 39: case 48: case 57: case 75: std::cout<<"363 2\n"; break;
+            case 49: case 58: case 67: case 76: std::cout<<"484 2\n"; break;
+            case 69: case 78: std::cout<<"4884 4\n"; break;
+            case 68: case 59: std::cout<<"1111 3\n"; break;
+            case 79: std::cout<<"44044 6\n"; break;
+            default: std::cout<<((n/10)+(n%10))*10+((n/10)+(n%10))<<" 1\n"; break;
+        }
     }
-    
     return 0;
 }
